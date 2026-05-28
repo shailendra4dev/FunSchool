774a2673cb5a2c723b6f1c91053c1ea73f146ff0
Shailendra Singh <SESA838410@se.com>
2026-05-28 11:14:55 +0530

    Fetching data from API Gateway.

--- client/src/app/Posts.tsx
+++ client/src/app/Posts.tsx
@@ -1,28 +1,48 @@
+import { useEffect, useState } from "react";
+import { fetchPosts } from "./services";
+
+
+
 const Posts = () => {
+
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const result = await fetchPosts();
+        if (result?.success == true){
+          console.log('[API] getPosts success:', result.data);
+          setPosts(result.data.posts.quotes);
+        } else {
+          console.error('[API] getPosts failure: API returned unsuccessful response');
+        }
+      } catch (err) {
+        console.error('[API] getPosts failure:', err);
+      }
+    };
+    load();
+  }, []);
+
   return (
     <table>
         <thead>
           <tr>
-            <th>Course</th>
-            <th>Description</th>
+            <th>Author</th>
+            <th>Quote</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Math</td>
-            <td>Learn about numbers and equations.</td>
-          </tr>
-          <tr>
-            <td>Science</td>
-            <td>Explore the wonders of the natural world.</td>
-          </tr>
-          <tr>
-            <td>History</td>
-            <td>Discover the stories of the past.</td>
-          </tr>
+          {posts.map((post: any) => (
+            <tr key={post.id}>
+              <td>{post.author}</td>
+              <td>{post.quote}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
   )
 }
+            
 
 export default Posts
